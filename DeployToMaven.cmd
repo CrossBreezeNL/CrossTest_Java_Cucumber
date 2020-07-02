3966ad30d85e8a1a7abe5b44bb3bd06b486bd064
Willem Otten <willem@X-Breeze.com>
2020-07-02 22:24:25 +0200

    fixed mvn deploy cmd

--- DeployToMaven.cmd
+++ DeployToMaven.cmd
@@ -1,13 +1,17 @@
 cd Crosstest
-call mvn clean deploy -Prelease
+call mvn clean install
+call mvn deploy -Prelease
 cd ..
 cd CucumberDatacomparison
-call mvn clean deploy -Prelease
+call mvn clean install
+call mvn deploy -Prelease
 cd ..
 cd Execution\InformaticapowerCenter
-call mvn clean deploy -Prelease
+call mvn clean install
+call mvn deploy -Prelease
 cd ..\..
 cd CrossTestSteps
-call mvn clean deploy -Prelease
+call mvn clean install
+call mvn deploy -Prelease
 cd ..
 pause done
