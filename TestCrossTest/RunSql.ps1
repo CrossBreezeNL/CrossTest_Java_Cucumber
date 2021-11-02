59fea6f4e9da465135224ea050cfa300e6a2e0ed
refai <refai@x-breeze.com>
2021-11-02 11:44:12 +0100

    runTestsforTag changed

--- TestCrossTest/RunSql.ps1
+++
@@ -1,4 +0,0 @@
-#wait for docker to be running 
-#sleep 15 
-#Create database TestDB
-docker exec xtestsqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Test1234! -i /temp/db/testdb.sql
--- TestCrossTest/runTestsforTag.cmd
+++ TestCrossTest/runTestsforTag.cmd
@@ -1,4 +1,4 @@
 set TAGS=%1
-mvn exec:java -B -Dexec.classpathScope=test -Dexec.mainClass=io.cucumber.core.cli.Main -Dexec.args="./src/test/resources --glue com.xbreeze.xtest.steps --tags '(%TAGS%)' --plugin 'pretty' --plugin 'json:target/unit-test-results.json' --plugin 'de.monochromata.cucumber.report.PrettyReports:target' "
+mvn exec:java -B -Dexec.classpathScope=test -Dexec.mainClass=io.cucumber.core.cli.Main -Dexec.args="./src/test/resources --glue com.xbreeze.xtest.steps --tags '(%TAGS%)' --plugin 'pretty' --plugin 'json:target/cucumber-unit.json' --plugin 'de.monochromata.cucumber.report.PrettyReports:target' "
 REM For some reason the command below is not executed when REM is removed.
 REM start chrome ./target/cucumber-html-reports/overview-features.html
