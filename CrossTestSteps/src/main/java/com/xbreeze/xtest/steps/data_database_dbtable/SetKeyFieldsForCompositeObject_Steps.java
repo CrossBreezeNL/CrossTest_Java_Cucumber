bc3741b193267b318b3269ea3ae3440fd2184ff8
Harmen Wessels <harmen@X-Breeze.com>
2021-11-18 17:27:45 +0100

    Regenerated steps to fix composite object issue.

--- CrossTestSteps/src/main/java/com/xbreeze/xtest/steps/data_database_dbtable/SetKeyFieldsForCompositeObject_Steps.java
+++ CrossTestSteps/src/main/java/com/xbreeze/xtest/steps/data_database_dbtable/SetKeyFieldsForCompositeObject_Steps.java
@@ -14,7 +14,7 @@ public class SetKeyFieldsForCompositeObject_Steps
         _DbTable_helper = DbTable_helper;
     }
 
-    @When("^I set \\(.*\\) as key for object (.+)$")
+    @When("^I set (\\(.*\\)) as key for object (.+)$")
     public void ISetKeyForCompositeObject(
         String key_fields,
         String composite_object
